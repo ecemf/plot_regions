15b734fe69c63cc776e80553d4ce5fb3ba75bba0
Will Usher <wusher@kth.se>
2022-11-22 14:19:43 +0100

    Update readme with description and remove axes

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # plot_regions
+
 Plot thumbnails of regional aggregations
+
+## Data
+
+Download NUTS boundaries from [GISCO](https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts#nuts21)
+
+## Dependencies
+
+These scripts rely upon the following Python libraries
+
+- matplotlib
+- pandas
+- geopandas
+
+## Use
+
+To use the script, first define a dictionary of country mappings using ISO-3166 two-letter country codes
+as the key, and the mapped region (e.g. model specific region) as the value.
+
+```python
+from plot_regions import main
+
+regions = {
+        'AT' : 'WEU',
+        'BE' : 'WEU',
+        'BG' : 'EEU',
+        'HR' : 'EEU',
+        'CY' : 'EEU',
+        'CZ' : 'EEU',
+        'DK' : 'WEU',
+        'EE' : 'EEU',
+        'FI' : 'SCA',
+        'FR' : 'WEU',
+        'DE' : 'WEU',
+        'GR' : 'WEU',
+        'HU' : 'EEU',
+        'IE' : 'WEU',
+        'IT' : 'WEU',
+        'LV' : 'EEU',
+        'LT' : 'EEU',
+        'LU' : 'WEU',
+        'MT' : 'WEU',
+        'NL' : 'WEU',
+        'NO' : 'SCA',
+        'PL' : 'EEU',
+        'PT' : 'WEU',
+        'RO' : 'EEU',
+        'SK' : 'EEU',
+        'SI' : 'EEU',
+        'ES' : 'WEU',
+        'SE' : 'SCA',
+        'UK' : 'WEU',
+        }
+
+main(regions)
+```
+
+The result is an thumbnail image of European regions colour coded by aggregation.
+
+![](example.png)
--- plot_regions.py
+++ plot_regions.py
@@ -15,7 +15,7 @@ import pandas as pd
 import geopandas as gpd
 import os
 
-DATA_DIR = ''
+DATA_DIR = 'data'
 
 def main(regions):
     """
@@ -25,7 +25,7 @@ def main(regions):
     regions : dict
         A nester dictionary of region aggregations
     """
-    path_rg = os.path.join(DATA_DIR + "NUTS_RG_60M_2021_3035.geojson")
+    path_rg = os.path.join(DATA_DIR, "NUTS_RG_60M_2021_3035.geojson")
     gdf_rg = gpd.read_file(path_rg)
     gdf_rg.crs = "EPSG:3035"
 
@@ -37,15 +37,43 @@ def main(regions):
     gdf_rg.plot(gdf_rg.region, ax=ax, figsize=(20,15), cmap='Accent',
                 missing_kwds={'color': 'lightgrey'}, legend=True)
     ax.set_xlim(0.2e7, 0.8e7)
-    ax.set_ylim(1.0e6, 6.0e6)
-    fig.savefig('test.png')
+    ax.set_ylim(1.2e6, 5.6e6)
+    ax.axis('off')
+    fig.savefig('example.png', bbox_inches='tight')
 
 
 def test_main():
 
     regions = {
+        'AT' : 'WEU',
+        'BE' : 'WEU',
+        'BG' : 'EEU',
+        'HR' : 'EEU',
+        'CY' : 'EEU',
+        'CZ' : 'EEU',
+        'DK' : 'WEU',
+        'EE' : 'EEU',
+        'FI' : 'SCA',
         'FR' : 'WEU',
-        'DE': 'EEU'
+        'DE' : 'WEU',
+        'GR' : 'WEU',
+        'HU' : 'EEU',
+        'IE' : 'WEU',
+        'IT' : 'WEU',
+        'LV' : 'EEU',
+        'LT' : 'EEU',
+        'LU' : 'WEU',
+        'MT' : 'WEU',
+        'NL' : 'WEU',
+        'NO' : 'SCA',
+        'PL' : 'EEU',
+        'PT' : 'WEU',
+        'RO' : 'EEU',
+        'SK' : 'EEU',
+        'SI' : 'EEU',
+        'ES' : 'WEU',
+        'SE' : 'SCA',
+        'UK' : 'WEU',
         }
 
     actual = main(regions)
